cf0cf73c40d5052b7242a2edc65f89a0e6913d8c
javiergespejo <javiergespejo@gmail.com>
2020-09-16 23:13:04 -0300

    Post model props has been modified
    new migrations added and database has been updated

---
+++ BlogChallenge/Migrations/20200916125311_Post prop ImageName added.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlogChallenge.Migrations
+{
+    public partial class PostpropImageNameadded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PostDTO");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageName",
+                table: "Post",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageName",
+                table: "Post");
+
+            migrationBuilder.CreateTable(
+                name: "PostDTO",
+                columns: table => new
+                {
+                    PostId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PostTitle = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PostDTO", x => x.PostId);
+                });
+        }
+    }
+}
--- BlogChallenge/Models/Post.cs
+++ BlogChallenge/Models/Post.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,11 +15,14 @@ namespace BlogChallenge.Models
         public string Title { get; set; }
         [Required]
         public string Content { get; set; }
+        [NotMapped]
+        public IFormFile Image { get; set; }
+        public string ImageName { get; set; }
         [Required]
-        public byte[] Image { get; set; }
-        [Required]
+        [Display(Name = "Category")]
         public int CategoryId { get; set; }
         public Category Category { get; set; }
+        [Display(Name = "Date")]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime CreationDate { get; set; }
     }
